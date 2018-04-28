206f981b261a87dacb017ce0c2fb33c6ab2809fa
Valery Chen <cpan3@stuy.edu>
2018-04-27 20:41:38 -0400

    forgot to add....

--- comprende.py
+++ comprende.py
@@ -1,3 +1,7 @@
+#Carol Pan
+#SoftDev2 pd7
+#K15: Do You Even List?
+#2018-04-26
 '''
 Ye Olde Password Rater
 
@@ -14,11 +18,32 @@ def pass_check(pwd):
     low = [1 for x in pwd if x in LC_LETTERS]
     cap = [1 for x in pwd if x in UC_LETTERS]
     num = [1 for x in pwd if x in NUMBERS]
-    #spe = [1 for x in pwd if x in SPEC_CHAR]
-    return len(low) >0 and len(cap)>0 and len(num)>0 #and len(spe)>0
+    return len(low) >0 and len(cap)>0 and len(num)>0
 
                         #check 1: no special characters   #check 2
 print pass_check("YOHO")  #f                                #f
 print pass_check('yohO')  #f                                #f
 print pass_check('yoh0')  #f                                #f
 print pass_check('Y0hoOO')#t                                #f
+
+
+def pass_strength(pwd):
+    strength = 0
+    
+    spe = [1 for x in pwd if x in SPEC_CHAR]
+    if len(spe) > 0:
+        strength += 1
+
+    if pass_check(pwd):
+        strength += 3
+
+    strength += len(pwd)//2
+
+    if strength > 10:
+        return 10
+    else:
+        return strength
+
+print pass_strength("tr0ub;DOR") #8
+print pass_strength("correcthorsebatterystaple@ABC123")#10
+print pass_strength(";bc123AB") #8
